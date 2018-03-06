75d72ff921135b3e903722131bdeb4ab26607745
Victor Timofte <victor.timofte98@gmail.com>
2018-03-06 14:09:24 +0200

    Fixed errors with getImage()

--- resources/views/email/cart.blade.php
+++ resources/views/email/cart.blade.php
@@ -6,7 +6,7 @@
 
 @foreach($products as $product)
 <ul>
-    <img width="120" src="{{$protocol }}{{$_SERVER['HTTP_HOST']}}/{{ $product->getImage() }}">
+    <img width="120" src="{{$protocol }}{{$_SERVER['HTTP_HOST']}}/{{ $product->getImage($product->id) }}">
     <li style='padding: 3px'>{{__("Title Product") }}: {{$product->title }}</li>
     <li style='padding: 3px'>{{__("Description Product") }}: {{$product->description }}</li>
     <li style='padding: 3px'>{{__("Price Product") }}: {{$product->price }}</li>
